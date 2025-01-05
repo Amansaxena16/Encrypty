c6c00f3eda3197011836b2451119ce62ed68d6a1
Amansaxena16 <aman16nov.as@gmail.com>
2025-01-06 00:21:44 +0530

    added new functionality

--- main.py
+++ main.py
@@ -1,32 +1,69 @@
 from cryptography.fernet import Fernet
 
-
-def takePath():
-    path = input('Enter the Path of the File : ')  # Take the path of the file from the user
-    return path
-    
-    
-def encryptFile(path):
+def readKey():
     with open('key.key','rb') as key_file:  # Open the file as binary mode
         key = key_file.read()
         
-    fernet = Fernet(key)  # Create the Fernet Object
+    return key
+
+def encryptFile(path,key):
     
     with open(path,'rb') as file:  # Open the file to read it in binary mode
         originalFile = file.read()
+        
+    fernet = Fernet(key)
+    
+    try:
+        fernet.decrypt(originalFile) # Check if the file is already encrypted
+        print('File is already encrypted')
+        return
+    except Exception as e:
+        pass
         
     encrypt = fernet.encrypt(originalFile)  # Encrypt the file
     
     with open(path,'wb') as encryptedFile:   # Open the file to write it in binary mode
         encryptedFile.write(encrypt)
+        
     
+def decryptFile(path,key):
     
-def decryptFile(path):
-    pass
+    fernet = Fernet(key)
+    
+    with open(path,'rb') as encryptedFile:
+        encrypted_Content = encryptedFile.read()
+        
+    try:
+        fernet.decrypt(encrypted_Content)  # Check if the file is already decrypted
+        print('File is already decrypted')
+    except Exception as e:
+        pass
+    
+    try:    
+        decrypted = fernet.decrypt(encrypted_Content) # Decrypt the file
+
+        with open(path,'wb') as decryptedFile: # Open the file to write it in binary mode
+            decryptedFile.write(decrypted)
+    except Exception as e:
+        print(f"Invalid Key: {e}")
+        
 
 def main():
-    path = takePath()
-    encryptFile(path)
+    
+    path = '/home/aman/Work/Encrypty/test.txt'
+    operation = input('Do you want to Encrypt(e) or Decrypt(d) the file : ').lower()
+    
+    try:
+        if(operation != 'e' and operation != 'd'):
+            raise ValueError('Invalid Input')
+        if(operation == 'e'):
+            key = readKey()
+            encryptFile(path,key)
+        else:
+            key = readKey()
+            decryptFile(path,key)   
+    except Exception as e:
+        print(f"An Error Occured : {e}")
     
     
 main()
